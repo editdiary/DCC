33919e25aa4791f18048b917986e4eae87897297
editdiary <henhoo886@gmail.com>
2024-10-13 18:33:26 +0900

    clear mission2-1

--- Mission2/README.md
+++ Mission2/README.md
@@ -4,3 +4,12 @@
 > [Mission 2-1] 해결했습니다. 자세한 코드는 `mission2-1.ipynb`를 확인해주세요<br>
 > 결과 파일은 1-1처럼 추후 작성해야 할 "성별&스타일 통계치" 작성을 위해 csv로 저장해두었습니다.<br>
 > 마찬가지로 train과 validation 각각 저장하였으니 확인해보시면 좋을 거 같습니다.
+
+## Update 2 (2024/10/13)
+> [Mission 2-1] 다시 해결했습니다.<br>
+> 이때까지 저희가 많은 질문과 논의를 거치면서 유효한 데이터에 대한 기준을 다시 적립하였고, 현재 코드는 `이미지와 파일명이 동일한` label 데이터에 대해서만 filtering이 진행되었습니다.<br>
+> `mission2-1.ipynb` 파일에 자세히 정리를 했습니다.<br>
+> 유효한 label만 filtering 하는 코드는 mission2-1.ipynb 파일에 포함되어 있으나, `filtering_labels.py`도 따로 실행할 수 있도록 코드를 변경해두겠습니다.
+
+> 유효한 label data를 필터링 후 분석의 코드는 구글드라이브 계정2에서 영채의 Mission1-1 코드를 바탕으로 작성되었습니다.<br>
+> `training_df.csv`는 유효한 labeling 파일의 메타데이터가 정리된 파일이며, `training_count_data.csv`는 유효한 데이터에 대한 "성별&스타일"별 통계치입니다. 즉, 2-1 문제의 결과입니다.
--- filtering_labels.py
+++ filtering_labels.py
@@ -1,71 +1,63 @@
 import os
 import shutil
+from tqdm import tqdm
 
-def process_dataset(src_image, src_label):
-    # src_folder 지정
-    img_folder = os.path.join('./dataset', src_image)
-    lbl_folder = os.path.join('./dataset', src_label)
+def filtering_label(dest_dir, state='train'):
+    # 원본 데이터셋 경로
+    src_folder = "C:/Users/prohe/OneDrive/Desktop/github_manage/DCC/dataset"
 
-    # 필터링된 label을 저장할 폴더 생성
-    filtered_folder = "./filtered_labels"
-    if not os.path.exists(filtered_folder):
-        os.makedirs(filtered_folder)
+    if state == 'train':
+        img_folder = 'training_image'
+        lbl_folder = 'training_label'
+    elif state == 'validation':
+        img_folder = 'validation_image'
+        lbl_folder = 'validation_label'
 
-    # 하위 폴더(training_label or validation_label) 생성
-    dest_dir = os.path.join(filtered_folder, src_label)
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
+    src_path = os.path.join(src_folder, lbl_folder)
+    dest_path = os.path.join(dest_dir, lbl_folder)
+
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
 
     # 오류 발생을 대비하여 처리된 이미지 수 추적
+    copy_count = 0
+    filtered_count = 0
     processed_count = 0
     error_count = 0
 
-    # img 폴더에서 고유한 이미지 ID 추출(중복을 막기 위해 set 사용)
-    image_ids = set()
-    for filename in os.listdir(img_folder):
-        if filename.endswith('.jpg'):
-            image_id = filename.split('_')[1]
-            image_ids.add(image_id)
+    image_lists = os.listdir(os.path.join(src_folder, img_folder))
+    image_lists = [x.split('.')[0] for x in image_lists]
 
-    # label 폴더의 json 파일 처리
-    for filename in os.listdir(lbl_folder):
-        if filename.endswith('.json'):
-            try:
-                # label 파일에서 이미지 ID 추출
-                image_id = filename.split('_')[1]
+    label_lists = os.listdir(src_path)
 
-                # label의 이미지 ID가 목록에 있는 경우 파일을 처리
-                src_path = os.path.join(lbl_folder, filename)
-                if image_id in image_ids:
-                    # 파일을 복사하여 저장
-                    dest_path = os.path.join(dest_dir, filename)
-                    shutil.copy2(src_path, dest_path)
-                else:
-                    # 유효하지 않은 이미지 ID인 경우 invalid 폴더에 저장
-                    invalid_dir = os.path.join(dest_dir, "invalid")
-                    if not os.path.exists(invalid_dir):
-                        os.makedirs(invalid_dir)
+    for file in tqdm(label_lists, desc=f"Processing {lbl_folder}", unit='file'):
+        lbl_filename = file[:-12]
 
-                    dest_path = os.path.join(invalid_dir, filename)
-                    shutil.copy2(src_path, dest_path)
-                
-                # 정상적으로 처리하였을 경우 count 증가
+        src_file_path = os.path.join(src_path, file)
+        dest_file_path = os.path.join(dest_path, file)
+
+        try:
+            if lbl_filename in image_lists:
+                shutil.copy2(src_file_path, dest_file_path)
                 processed_count += 1
-            
-            except Exception as e:
-                # 에러가 발생하여 변환에 실패하면 에러 메시지 출력
-                error_count += 1
-                print(f"Error processing {filename}: {str(e)}")
+                copy_count += 1
+            else:
+                filtered_count += 1
+                continue
+        except Exception as e:
+            error_count += 1
+            print(f"Error: {e}")
 
-    print(f"Labels are filtered successfully! ==> Total labels processed: {processed_count}/{len(os.listdir(lbl_folder))} | Total labels failed: {error_count}/{len(os.listdir(lbl_folder))}")
+    print(f"\nFolder: {lbl_folder}")
+    print(f"Filtered files: {filtered_count}, Copied files: {copy_count}")
+    print(f"Processed files: {processed_count}, Error encountered: {error_count}\n")
 
 
 if __name__ == "__main__":
-    # 데이터셋 경로 설정
-    src_train_img = 'training_image'
-    src_train_label = 'training_label'
-    src_val_img = 'validation_image'
-    src_val_label = 'validation_label'
+    # 필터링된 라벨을 저장할 디렉토리
+    dest_dir = "C:/Users/prohe/OneDrive/Desktop/github_manage/DCC/temp"
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
 
-    process_dataset(src_train_img, src_train_label)
-    process_dataset(src_val_img, src_val_label)
+    filtering_label(dest_dir, state='train')
+    filtering_label(dest_dir, state='validation')
